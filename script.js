60127e81ce3b1537ca620a7ccfe2ce37123aa744
Raimundo <Raimundosegund17@outlook.com.br>
2024-10-07 15:02:45 -0300

    Implemented javascript function to change color scheme, added the function to the button and made a css class for the theme

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Implement function to switch the website colors
+function lightswitch(){
+    const body = document.body;
+    const themeswitch = document.getElementById("light-switch");
+    if (body.classList.contains("light-theme")){
+        body.classList.remove("light-theme");
+        themeButton.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--main-bg-color');
+    } else {
+        body.classList.add("light-theme");
+        themeButton.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--main-bg-color');
+    }
+}
